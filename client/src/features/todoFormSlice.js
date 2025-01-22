698c4364b0d9361426b725982dff209364edf41d
somil19 <somilagarwal2@gmail.com>
2025-01-23 00:52:55 +0530

    update

--- client/src/features/todoFormSlice.js
+++ client/src/features/todoFormSlice.js
@@ -6,7 +6,7 @@ const initialValue = {
 
   todoTitle: "",
 
-  backendUrl: "http://localhost:5000",
+  backendUrl: import.meta.env.VITE_API_URL || "http://localhost:5000",
 };
 
 const todoFormSlice = createSlice({
